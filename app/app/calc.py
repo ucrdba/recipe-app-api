f1f91d04e5255f4d243088ed44b594641298a4b3
Bob Ayers <ucrdba@gmail.com>
2025-04-12 11:42:10 -0700

    codechecker version ph 41

--- app/app/calc.py
+++ app/app/calc.py
@@ -4,3 +4,10 @@ Calculator functions for the app.
 def add(x, y):
     """Add two numbers together."""
     return x + y
+
+
+
+
+def subtract(x, y):
+    """Subtract one number from another."""
+    return x - y
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,7 +1,7 @@
 """
 Sample tests
 """
-from django import SimpleTestCase
+from django.test import SimpleTestCase
 
 from app import calc
 
@@ -14,3 +14,9 @@ class CalcTests(SimpleTestCase):
         res = calc.add(5, 6)
        
         self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """Test subtracting numbers."""
+        res = calc.subtract(10, 15)
+
+        self.assertEqual(res, 5)
